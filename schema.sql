7718e07d61d0f1a37c7a153d22b99fdf9d14ff97
Arun Bajpai <arun.bajpai@aspora.com>
2026-04-30 16:59:07 +0530

    create dead_jobs on boot

--- schema.sql
+++ schema.sql
@@ -1,12 +1,25 @@
 CREATE TABLE IF NOT EXISTS jobs (
-                                    id TEXT PRIMARY KEY,
-                                    payload TEXT,
-                                    type TEXT,
-                                    duration INT,
-                                    status TEXT,
-                                    retries INT,
-                                    max_retries INT,
-                                    priority TEXT DEFAULT 'medium',
-                                    created_at TIMESTAMP DEFAULT now(),
+    id TEXT PRIMARY KEY,
+    payload TEXT,
+    type TEXT,
+    duration INT,
+    status TEXT,
+    retries INT,
+    max_retries INT,
+    priority TEXT DEFAULT 'medium',
+    created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now()
-    );
+);
+
+CREATE TABLE IF NOT EXISTS dead_jobs (
+    id UUID PRIMARY KEY,
+    payload TEXT NOT NULL,
+    type TEXT NOT NULL,
+    duration INTEGER NOT NULL,
+    retries INTEGER,
+    max_retries INTEGER,
+    priority TEXT,
+    failure_reason TEXT,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    failed_at TIMESTAMPTZ DEFAULT now()
+);
